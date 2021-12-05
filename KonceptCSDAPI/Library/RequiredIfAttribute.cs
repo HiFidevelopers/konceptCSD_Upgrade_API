bdfd7891e7f843203bacfb179c6de511f6bf4194
inder <DGR@DESKTOP-8JCQJ18>
2021-12-05 21:40:53 +0530

    commit

---
+++ KonceptCSDAPI/Library/RequiredIfAttribute.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KonceptSupportLibrary
+{
+    public class RequiredIfAttribute : ValidationAttribute
+    {
+        RequiredAttribute _innerAttribute = new RequiredAttribute();
+        public string _dependentProperty { get; set; }
+        public object _targetValue { get; set; }
+
+        public RequiredIfAttribute(string dependentProperty, object targetValue)
+        {
+            this._dependentProperty = dependentProperty;
+            this._targetValue = targetValue;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var field = validationContext.ObjectType.GetProperty(_dependentProperty);
+                if (field != null)
+                {
+                    var dependentValue = field.GetValue(validationContext.ObjectInstance, null);
+                    if (dependentValue != null)
+                    {
+                        if ((dependentValue == null && _targetValue == null) || (dependentValue.Equals(_targetValue)))
+                        {
+                            if (!_innerAttribute.IsValid(value))
+                            {
+                                string name = validationContext.DisplayName;
+                                return new ValidationResult(ErrorMessage = name + " Is required.");
+                            }
+                        }
+                    }
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(FormatErrorMessage(_dependentProperty));
+                }
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
+        }
+
+
+    }
+
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        int _minimumAge;
+
+        public MinimumAgeAttribute(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date))
+            {
+                return date.AddYears(_minimumAge) < DateTime.Now;
+            }
+
+            return false;
+        }
+    }
+
+    #region Inventory Range Validator
+    public class InventoryRangeAttribute : ValidationAttribute
+    {
+        RequiredAttribute _innerAttribute = new RequiredAttribute();
+        public string _dependentProperty { get; set; }
+
+        public InventoryRangeAttribute(string dependentProperty)
+        {
+            this._dependentProperty = dependentProperty;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var field = validationContext.ObjectType.GetProperty(_dependentProperty);
+            if (field != null)
+            {
+                var dependentValue = field.GetValue(validationContext.ObjectInstance, null);
+                if (Convert.ToInt64(dependentValue) == 1)
+                {
+                    if (long.TryParse(value.ToString(),out long result))
+                    {
+                        if (Convert.ToInt64(value) < 1 || Convert.ToInt64(value) > 99999999)
+                        {
+                            if (!_innerAttribute.IsValid(value))
+                            {
+                                return new ValidationResult(ErrorMessage = "Invalid Range");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return new ValidationResult(ErrorMessage = "Invalid Range");
+                    }
+                }
+                else if (Convert.ToInt64(dependentValue) == 2)
+                {
+                    if (long.TryParse(value.ToString(), out long result))
+                    {
+                        if (Convert.ToInt64(value) < 1 || Convert.ToInt64(value) > 9999999999)
+                        {
+                            if (!_innerAttribute.IsValid(value))
+                            {
+                                return new ValidationResult(ErrorMessage = "Invalid Range");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return new ValidationResult(ErrorMessage = "Invalid Range");
+                    }
+                }
+
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(FormatErrorMessage(_dependentProperty));
+            }
+        }
+    }
+    #endregion
+
+    #region Inventory String Length Validator
+    public class InventoryStringLengthAttribute : ValidationAttribute
+    {
+        RequiredAttribute _innerAttribute = new RequiredAttribute();
+        public string _dependentProperty { get; set; }
+
+        public InventoryStringLengthAttribute(string dependentProperty)
+        {
+            this._dependentProperty = dependentProperty;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var field = validationContext.ObjectType.GetProperty(_dependentProperty);
+            if (field != null)
+            {
+                var dependentValue = field.GetValue(validationContext.ObjectInstance, null);
+                if (Convert.ToInt64(dependentValue) == 1)
+                {
+                    if (value.ToString().Length < 7 || value.ToString().Length > 8)
+                    {
+                        if (!_innerAttribute.IsValid(value))
+                        {
+                            string name = validationContext.DisplayName;
+                            return new ValidationResult(ErrorMessage = "Length should be between 7 or 8");
+                        }
+                    }
+                }
+                else if (Convert.ToInt64(dependentValue) == 2)
+                {
+                    if (value.ToString().Length < 10 || value.ToString().Length > 10)
+                    {
+                        if (!_innerAttribute.IsValid(value))
+                        {
+                            string name = validationContext.DisplayName;
+                            return new ValidationResult(ErrorMessage = "Length should be 10");
+                        }
+                    }
+                }
+
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(FormatErrorMessage(_dependentProperty));
+            }
+        }
+    }
+    #endregion
+}
--- KonceptCSDAPI/Models/User/UserModels.cs
+++ KonceptCSDAPI/Models/User/UserModels.cs
@@ -5,134 +5,132 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using KonceptSupportLibrary;
 
 namespace KonceptCSDAPI.Models.User
 {
-	public class UserModels
-	{
-		//[Required(ErrorMessage = "User ID is required.")]
-		public Int64? User_ID { get; set; } = 0;
-		public Int64? Logged_User_ID { get; set; } = 0;
-	}
+    public class UserModels
+    {
+        //[Required(ErrorMessage = "User ID is required.")]
+        public Int64? User_ID { get; set; } = 0;
+        public Int64? Logged_User_ID { get; set; } = 0;
+    }
 
-	public class UserFilterModel
-	{
-		public Int64? User_ID { get; set; } = 0;
-		public string? Search { get; set; } = string.Empty;
-		public string User_Type { get; set; } = string.Empty;
-		public Int64? User_Group_ID { get; set; } = 0;
-		public Boolean? Is_Active { get; set; } = true;
-		public Int64? Logged_User_ID { get; set; } = 0;
-	}
-
-
-	public class UserInsertUpdateModel
-	{
-
-		//User Info
-		public Int64? User_ID { get; set; } = 0;
-
-		[Required(ErrorMessage = "User Type is required.")]
-		public string User_Type { get; set; }
-
-		[Required(ErrorMessage = "Parent(Organization) User is required.")]
-		public Int64 Parent_User_ID { get; set; }
-
-		public string? User_Description { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = "User Group is required.")]
-		public Int64 User_Group_ID { get; set; }
-		public string? FullName { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = "First Name is required.")]
-		public string FirstName { get; set; }
-		public string? LastName { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = "Gender is required.")]
-		public int Gender { get; set; }
-
-		[Required(ErrorMessage = "Email is required.")]
-		public string Email { get; set; }
-		public Boolean? Is_Email_Verify { get; set; } = true;
-
-		[Required(ErrorMessage = "MobileNo is required.")]
-		public string MobileNo { get; set; }
-
-		public Boolean? Is_Mobile_Verify { get; set; } = true;
-		public string? Valid_till { get; set; } = string.Empty;
-		public Boolean? Is_Active { get; set; } = true;
+    public class UserFilterModel
+    {
+        public Int64? User_ID { get; set; } = 0;
+        public string? Search { get; set; } = string.Empty;
+        public string User_Type { get; set; } = string.Empty;
+        public Int64? User_Group_ID { get; set; } = 0;
+        public Boolean? Is_Active { get; set; } = true;
+        public Int64? Logged_User_ID { get; set; } = 0;
+    }
 
 
-		//User Login Info
-		[Required(ErrorMessage = "Username is required.")]
-		public string Username { get; set; }
+    public class UserInsertUpdateModel
+    {
+
+        //User Info
+        public Int64? User_ID { get; set; } = 0;
+
+        [Required(ErrorMessage = "User Type is required.")]
+        public string User_Type { get; set; }
+
+        [Required(ErrorMessage = "Parent(Organization) User is required.")]
+        public Int64 Parent_User_ID { get; set; }
+
+        public string? User_Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "User Group is required.")]
+        public Int64 User_Group_ID { get; set; }
+        public string? FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+        public string? LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Gender is required.")]
+        public int Gender { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+        public Boolean? Is_Email_Verify { get; set; } = true;
+
+        [Required(ErrorMessage = "MobileNo is required.")]
+        public string MobileNo { get; set; }
+
+        public Boolean? Is_Mobile_Verify { get; set; } = true;
+        public string? Valid_till { get; set; } = string.Empty;
+        public Boolean? Is_Active { get; set; } = true;
+
+        //User Login Info
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [RequiredIf("User_ID", "0", ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        //User Profile Info
+        public string Profile_Pic { get; set; }
+        public string? Address { get; set; } = string.Empty;
+        public string? Address_Other { get; set; } = string.Empty;
+        public string? City { get; set; } = string.Empty;
+        public Int64? State_ID { get; set; } = 0;
+        public Int64? Country_ID { get; set; } = 0;
+        public string? Zip_Code { get; set; } = string.Empty;
+        public string? Facebook_Profile_URL { get; set; } = string.Empty;
+        public string? LinkedIn_Profile_URL { get; set; } = string.Empty;
+        public Int64? Logged_User_ID { get; set; } = 0;
+
+    }
 
 
-		[Required(ErrorMessage = "Password is required.")]
-		public string Password { get; set; }
+    public class UserGroupFilterModel
+    {
+        public Int64? User_Group_ID { get; set; } = 0;
+        public string? Search { get; set; } = string.Empty;
+        public string? User_Group_Name { get; set; } = string.Empty;
+        public Boolean? Is_Predefined { get; set; } = true;
+        public Boolean? Is_Active { get; set; } = true;
+        public Int64? Logged_User_ID { get; set; } = 0;
+    }
 
 
-		//User Profile Info
-		public string Profile_Pic { get; set; }
-		public string? Address { get; set; } = string.Empty;
-		public string? Address_Other { get; set; } = string.Empty;
-		public string? City { get; set; } = string.Empty;
-		public Int64? State_ID { get; set; } = 0;
-		public Int64? Country_ID { get; set; } = 0;
-		public string? Zip_Code { get; set; } = string.Empty;
-		public string? Facebook_Profile_URL { get; set; } = string.Empty;
-		public string? LinkedIn_Profile_URL { get; set; } = string.Empty;
-		public Int64? Logged_User_ID { get; set; } = 0;
+    public class UserGroupInsertUpdateModel
+    {
+        public Int64? User_Group_ID { get; set; } = 0;
 
-	}
+        [Required(ErrorMessage = "User Group Name is required.")]
+        public string User_Group_Name { get; set; }
 
+        [Required(ErrorMessage = "User Group Description is required.")]
+        public string User_Group_Description { get; set; }
+        public Boolean? Is_Predefined { get; set; } = false;
+        public Boolean? Is_Active { get; set; } = true;
 
-	public class UserGroupFilterModel
-	{
-		public Int64? User_Group_ID { get; set; } = 0;
-		public string? Search { get; set; } = string.Empty;
-		public string? User_Group_Name { get; set; } = string.Empty;
-		public Boolean? Is_Predefined { get; set; } = true;
-		public Boolean? Is_Active { get; set; } = true;
-		public Int64? Logged_User_ID { get; set; } = 0;
-	}
+        //User Group Access Area Mapping List
+        [Required(ErrorMessage = "User Group Access Area Mapping List is required.")]
+        public List<AccessAreaList> AccessAreaList { get; set; }
+        public Int64? Logged_User_ID { get; set; } = 0;
+
+    }
 
 
-	public class UserGroupInsertUpdateModel
-	{
-		public Int64? User_Group_ID { get; set; } = 0;
+    public class AccessAreaList
+    {
 
-		[Required(ErrorMessage = "User Group Name is required.")]
-		public string User_Group_Name { get; set; }
+        public Int64? User_Group_Access_Area_Mapping_ID { get; set; } = 0;
+        public Int64? User_Group_ID { get; set; } = 0;
 
-		[Required(ErrorMessage = "User Group Description is required.")]
-		public string User_Group_Description { get; set; }
-		public Boolean? Is_Predefined { get; set; } = false;
-		public Boolean? Is_Active { get; set; } = true;
+        [Required(ErrorMessage = "User Group Access Area ID is required for mapping.")]
+        public Int64? User_Group_Access_Area_ID { get; set; } = 0;
 
-		//User Group Access Area Mapping List
-		[Required(ErrorMessage = "User Group Access Area Mapping List is required.")]
-		public List<AccessAreaList> AccessAreaList { get; set; }
-		public Int64? Logged_User_ID { get; set; } = 0;
+        public Boolean? Is_Create { get; set; } = true;
+        public Boolean? Is_Retrieve { get; set; } = true;
+        public Boolean? Is_Update { get; set; } = true;
+        public Boolean? Is_Delete { get; set; } = true;
+        public Boolean? Is_Active { get; set; } = true;
 
-	}
-
-
-	public class AccessAreaList
-	{
-
-		public Int64? User_Group_Access_Area_Mapping_ID { get; set; } = 0;
-		public Int64? User_Group_ID { get; set; } = 0;
-
-		[Required(ErrorMessage = "User Group Access Area ID is required for mapping.")]
-		public Int64? User_Group_Access_Area_ID { get; set; } = 0;
-
-		public Boolean? Is_Create { get; set; } = true;
-		public Boolean? Is_Retrieve { get; set; } = true;
-		public Boolean? Is_Update { get; set; } = true;
-		public Boolean? Is_Delete { get; set; } = true;
-		public Boolean? Is_Active { get; set; } = true;
-
-	}
+    }
 
 }
